d3913dbb1af424f5794cd2e8274e3d5623954c91
jinwoo <upbreak05@kakao.com>
2024-03-08 13:46:31 +0900

    kakao 주소검색 api 구현하기
    카카오에서 제공하는 restapi 문서에 맞게 구현을 해보았다.
    
    ## ref
    https://developers.kakao.com/docs/latest/ko/local/dev-guide

---
+++ src/main/java/com/jinwoo/dev/pharmacyrecommendation/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,44 @@
+package com.jinwoo.dev.pharmacyrecommendation.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+
+    private MetaDto metaDto;
+
+    private List<DocumentDto> documentList;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class MetaDto {
+
+        @JsonProperty("total_count")
+        private Integer totalCount;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class DocumentDto {
+
+        @JsonProperty("address_name")
+        private String addressName;
+
+        @JsonProperty("x")
+        private double longitude;
+
+        @JsonProperty("y")
+        private double latitude;
+
+    }
+
+}
